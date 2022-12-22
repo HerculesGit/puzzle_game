1a8272f93df17f0a3be6ac9d40926340f4da2d65
Hercules <hercules.silva@dcx.ufpb.br>
2022-12-21 21:34:33 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/herco/puzzle/puzzle_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.herco.puzzle.puzzle_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/screens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PuzzleGameTable extends StatelessWidget {
+  const PuzzleGameTable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
